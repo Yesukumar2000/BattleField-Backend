000bae7386819597aa1fc8da085fe04f80ad075f
yesukumar bhuvanagiri <121677945+Yesukumar2000@users.noreply.github.com>
2025-05-29 21:12:33 +0530

    updated images

--- api/server.js
+++ api/server.js
@@ -8,8 +8,8 @@ const serverInfo = {
   serverName: "! RC3-BASEMAPS",
   mapName: "CONQUEST LARGE - LANCANG DAM",
   gameMode: "CUSTOM - 6O HZ",
-  antiCheatInfo: "Server protected by The K-50 AntiCheat. Vi les, Questions, Request, Appeal, Visit us: www.eps.gg",
-  discordLink: "https://discord.gg/",
+  antiCheatInfo: "Server protected by The_K-50 AntiCheat. Vip !Rules, Questions, Request, Appeal, Visit us: www.epg.gg | Discord ",
+  discordLink: "https://discord.gg/3r5NK4d",
   players: "60/64",
   ping: "104ms",
   tickrate: "60 Hz",
@@ -48,14 +48,14 @@ const serverInfo = {
 
 // Mock data for map rotation
 const mapRotation = [
-  "SIEGE OF SHANGHAI",
-  "OPERATION LOCKER",
-  "DAWNBREAKER",
-  "ROGUE TRANSMISSION",
-  "ZAVOD 311",
-  "FLOOD ZONE",
-  "HAINAN RESORT",
-  "GOLMUD RAILWAY"
+  {"image":"https://cdn.prod.website-files.com/6013fff62154adaa4600f932/6039076de23d2b9d4e1fbf67_browser__SI-next-image%20-%2006.png","SIEGE OF SHANGHAI" },
+  {"image":"https://cdn.prod.website-files.com/6013fff62154adaa4600f932/6039076c1ab31e3d009e691e_browser__SI-next-image%20-%2002.png","OPERATION LOCKER" },
+  {"image":"https://cdn.prod.website-files.com/6013fff62154adaa4600f932/6039076d9b48792dbe261e07_browser__SI-next-image%20-%2004.png","DAWNBREAKER"},
+  {"image":"https://cdn.prod.website-files.com/6013fff62154adaa4600f932/6039076d8123214d0747f2a1_browser__SI-next-image%20-%2005.png", "ROGUE TRANSMISSION" },
+  {"image":"https://cdn.prod.website-files.com/6013fff62154adaa4600f932/6039076cab09f17a68176928_browser__SI-next-image%20-%2001.png", "ZAVOD 311"},
+  {"image":"https://cdn.prod.website-files.com/6013fff62154adaa4600f932/6039076d00244d9e5298944f_browser__SI-next-image%20-%2003.png", "FLOOD ZONE"},
+  {"image":"https://cdn.prod.website-files.com/6013fff62154adaa4600f932/6039076c1ab31e3d009e691e_browser__SI-next-image%20-%2002.png", "HAINAN RESORT"},
+  {"image":"https://cdn.prod.website-files.com/6013fff62154adaa4600f932/6039076cab09f17a68176928_browser__SI-next-image%20-%2001.png", "GOLMUD RAILWAY"}
 ];
 
 // Define your routes
@@ -73,26 +73,3 @@ app.get('/api/map-rotation', (req, res) => {
 
 // Export the express app for use in api/index.js
 module.exports = app;
-
-
-// const express = require('express');
-// const cors = require('cors');
-// const { createServer } = require('@vercel/node'); // Vercel specific
-
-// const app = express();
-// app.use(cors());
-
-
-// app.get('/', (req, res) => {
-//   res.send('Battlefield Backend Server is running');
-// });
-
-// app.get('/api/server-info', (req, res) => {
-//   res.json(serverInfo);
-// });
-
-// app.get('/api/map-rotation', (req, res) => {
-//   res.json(mapRotation);
-// });
-
-// module.exports = app;
